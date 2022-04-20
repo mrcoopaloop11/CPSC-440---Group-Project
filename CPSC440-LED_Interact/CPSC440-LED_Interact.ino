a3b4f0aa9391f7c67f2af1aab7f6180fcf005dbe
Cooper Santillan <maggieandme117@gmail.com>
2022-04-20 14:40:42 -0700

    comments

--- CPSC440-LED_Interact/CPSC440-LED_Interact.ino
+++ CPSC440-LED_Interact/CPSC440-LED_Interact.ino
@@ -42,12 +42,24 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   // initialize serial communication:
   Serial.begin(9600);
+
+  //cycle through all available colors
+  //
 }
 
 
 void loop() {
   // read the pushbutton input pin:
   buttonState = digitalRead(buttonPin);
+
+  // get the time in milli and use modulo of [random value ; like 12] to see if it equals 0
+      // get random color function
+      // timer set it to zero if color is green
+            // if not, keep the timer going
+
+  // keep color for so many cycles, then turn off rgb leds again
+        // if its green, then call the playerLOSES function
+      
 
   // compare the buttonState to its previous state
   if (buttonState != lastButtonState) {
@@ -58,6 +70,9 @@ void loop() {
       Serial.println("on");
       Serial.print("number of button pushes: ");
       Serial.println(buttonPushCounter);
+
+      // if green within a second, call the win function
+      // if not green OR green but more than a second, call the lose function
     } else {
       // if the current state is LOW then the button went from on to off:
       Serial.println("off");
@@ -69,19 +84,51 @@ void loop() {
   lastButtonState = buttonState;
 
 
+  
+
   // turns on the LED every four button pushes by checking the modulo of the
   // button push counter. the modulo function gives you the remainder of the
   // division of two numbers:
   if (buttonPushCounter % 4 == 0) {
-    digitalWrite(ledPin, HIGH);
+    changeLED(true, true, true);        // white (all on)
   } else {
-    digitalWrite(ledPin, LOW);
+    changeLED(false, false, false);     // black (all off)
   }
 
 }
 
 
-void randomColor() {
+
+void changeLED(bool red, bool blue, bool green) {
+    int redState;
+    int blueState;
+    int greenState;
   
+    if (red == true) redState == HIGH; else redState = LOW;
+    if (blue == true) blueState == HIGH; else blueState = LOW;
+    if (green == true) greenState == HIGH; else greenState = LOW;
+    
+    digitalWrite(redLEDPin, redState);
+    digitalWrite(blueLEDPin, blueState);
+    digitalWrite(greenLEDPin, greenState);
+}
+
+
+void randomColor() {
+  // get a random value from 1 to 6
+
+  // switch 1 to 6 case (red, green, blue, cyan, magenta, yellow)
+  // green case, change the global variable for G_WINNING
+}
+
+
+void playerWINS() {
+  // cycle through all the colors for x amount of seconds
+}
+
+
+void playerLOSES() {
+  // flash red light for x amount of seconds
+  // call the restart game function
 }
 }
